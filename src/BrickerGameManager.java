0cf9f97c49cbbcd7d08cb48fc6f8c55e95ddb8df
omrishlomy <omrishlomy44@gmail.com>
2025-11-18 13:34:26 +0200

    first commit

---
+++ src/BrickerGameManager.java
@@ -0,0 +1,11 @@
+import danogl.GameManager;
+import danogl.util.Vector2;
+
+
+public class BrickerGameManager extends GameManager{
+
+ public static void main(String[] args) {
+   GameManager gameManager =new GameManager(" gameManager example =",new Vector2(700,500));
+   gameManager.run();
+  }
+ }
