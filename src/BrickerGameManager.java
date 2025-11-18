ada2e063ec2492055e9632fc3a1d3387e0646a80
lihi.sofri <lihi.sofri@mail.huji.ac.il>
2025-11-18 13:41:08 +0200

    try

--- src/BrickerGameManager.java
+++ src/BrickerGameManager.java
@@ -7,5 +7,7 @@ public class BrickerGameManager extends GameManager{
  public static void main(String[] args) {
    GameManager gameManager =new GameManager(" gameManager example =",new Vector2(700,500));
    gameManager.run();
+   // trying push
+     int zero = 0;
   }
  }
