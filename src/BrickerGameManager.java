d47ae1596b9089d24d3e384e6ef1a0b9b59de0fb
omrishlomy <omrishlomy44@gmail.com>
2025-11-18 13:49:10 +0200

    first commit

--- src/BrickerGameManager.java
+++ src/BrickerGameManager.java
@@ -7,13 +7,6 @@ public class BrickerGameManager extends GameManager{
  public static void main(String[] args) {
    GameManager gameManager =new GameManager(" gameManager example =",new Vector2(700,500));
    gameManager.run();
-   int zero = 0;
-   int one = 1;
-   int two = 2;
-   int three = 3;
-   int four = 4;
-   int five = 5;
-   int six = 6;
-   int seven = 7;
+
   }
  }
