a9eac9bd30dae6513282baf2c4395d5a50099990
omrishlomy <omrishlomy44@gmail.com>
2025-11-29 19:03:00 +0200

    first commit

--- src/bricker/brick_strategies/CollisionStrategy.java
+++ src/bricker/brick_strategies/CollisionStrategy.java
@@ -8,7 +8,17 @@ import danogl.collisions.Collision;
  * @author Lihi
  */
 public interface CollisionStrategy {
+ /**
+  * onCollison method to be implemet by the classes implementing the interface
+   * @param thisObj
+  * @param otherObj
+  */
     public void onCollision(GameObject thisObj, GameObject otherObj);
+
+ /**
+  *
+  * @return the current strategies chain length
+  */
 	default int countDecorators(){
 	 return 0;
 	}
